094a70bd2d55e0be2c939aedf3396a19e629b975
asdf49514 <47475531+1135975331@users.noreply.github.com>
2021-01-06 01:26:56 +0900

    체력바를 저장할 공간을 마련 (현재 파일 or 클래스에 구애받지 않고 접근할 수 있도록)

--- src/main/kotlin/DisplayHealthBar/BossbarList.kt
+++ src/main/kotlin/DisplayHealthBar/BossbarList.kt
@@ -1,9 +1,12 @@
 package DisplayHealthBar
 
 import org.bukkit.boss.BossBar
+import org.bukkit.entity.Player
 import java.util.HashMap
 
 object BossbarList
 {
-
+	var playerList = HashMap<String, Player>()
+	var bossBarList = HashMap<String, BossBar>()
+	var taskID = HashMap<String, Int>()
 }
