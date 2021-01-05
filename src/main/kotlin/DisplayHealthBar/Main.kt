776640a1d6e5cdb54bf1551124a313e0f953c794
asdf49514 <47475531+1135975331@users.noreply.github.com>
2021-01-06 01:29:49 +0900

    플러그인 리로드 시 날라가 버린 체력바를 현재 접속한 플레이어들에 대해 저장하는 코드 추가

--- src/main/kotlin/DisplayHealthBar/Main.kt
+++ src/main/kotlin/DisplayHealthBar/Main.kt
@@ -2,6 +2,8 @@ package DisplayHealthBar
 
 import DisplayHealthBar.Event.EntityBossBarHealthEvents
 import org.bukkit.Bukkit
+import org.bukkit.boss.BarColor
+import org.bukkit.boss.BarStyle
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin()
@@ -15,6 +17,16 @@ class Main : JavaPlugin()
 		getCommand("healthBar")?.setExecutor(commands)
 		CommandManager(this)  //?? 이거 없어도 되지 않음?
 		Bukkit.getPluginManager().registerEvents(EntityBossBarHealthEvents(), this)  //이벤트 코드 파일을 지정함
+		
+		
+		for(player in Bukkit.getOnlinePlayers()) {
+			BossbarList.playerList[player.name] = player
+			BossbarList.bossBarList[player.name] = Bukkit.createBossBar("Health Bar", BarColor.GREEN, BarStyle.SEGMENTED_10)
+			BossbarList.bossBarList[player.name]!!.addPlayer(player)
+			BossbarList.bossBarList[player.name]!!.isVisible = false
+			BossbarList.taskID[player.name] = Math.random().toInt()
+		}
+		
 		println("§6<<체력바 표시 플러그인 by asdf49514>>\n§a플러그인이 성공적으로 활성화 되었습니다.§f")
 	}
 	
