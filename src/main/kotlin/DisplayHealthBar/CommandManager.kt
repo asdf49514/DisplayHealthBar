75e70ec924032e58096f0259c1f7316e72251a1d
asdf49514 <47475531+1135975331@users.noreply.github.com>
2021-01-06 01:27:22 +0900

    체력바를 리로드하거나 모두 삭제하는 명령어 추가

--- src/main/kotlin/DisplayHealthBar/CommandManager.kt
+++ src/main/kotlin/DisplayHealthBar/CommandManager.kt
@@ -14,27 +14,33 @@ class CommandManager(private var plugin: Main) : CommandExecutor {  //todo 빌�
 	override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
 		if(command.name == "healthBar") {
 			when {
-//				args[0].equals("addAllPlayers", true) -> {  //작동안함
-//					val entityBossBarHealthEvents = EntityBossBarHealthEvents()
-//					for(player in Bukkit.getOnlinePlayers()) {
-//						entityBossBarHealthEvents.playerList[player.name] = player
-//						entityBossBarHealthEvents.bossBarList[player.name] = Bukkit.createBossBar("Health Bar", BarColor.GREEN, BarStyle.SEGMENTED_10)
-//						entityBossBarHealthEvents.bossBarList[player.name]!!.addPlayer(player)
-//						entityBossBarHealthEvents.bossBarList[player.name]!!.isVisible = false
-//					}
-//				}
-//				args[0].equals("delAllPlayers", true) -> {
-//					val entityBossBarHealthEvents = EntityBossBarHealthEvents()
-//					for(player in Bukkit.getOnlinePlayers()) {
-//						try {
-//							entityBossBarHealthEvents.bossBarList[player.name]!!.removePlayer(player)
-//							entityBossBarHealthEvents.bossBarList.remove(player.name, entityBossBarHealthEvents.bossBarList[player.name]!!)
-//						}
-//						catch(exception: Exception) {
-//							println("bossBar를 찾을 수 없습니다.")
-//						}
-//					}
-//				}
+				args[0].equals("reloadAll", true) -> {  //작동안함
+					for(player in Bukkit.getOnlinePlayers()) {
+						try {
+							BossbarList.bossBarList[player.name]!!.removePlayer(player)
+							BossbarList.bossBarList.remove(player.name, BossbarList.bossBarList[player.name]!!)
+							BossbarList.taskID.remove(player.name)
+						} catch(ignored: Exception) { }
+						
+						BossbarList.playerList[player.name] = player
+						BossbarList.bossBarList[player.name] = Bukkit.createBossBar("Health Bar", BarColor.GREEN, BarStyle.SEGMENTED_10)
+						BossbarList.bossBarList[player.name]!!.addPlayer(player)
+						BossbarList.bossBarList[player.name]!!.isVisible = false
+						BossbarList.taskID[player.name] = Math.random().toInt()
+					}
+				}
+				args[0].equals("deleteAll", true) -> {
+					for(player in Bukkit.getOnlinePlayers()) {
+						try {
+							BossbarList.bossBarList[player.name]!!.removePlayer(player)
+							BossbarList.bossBarList.remove(player.name, BossbarList.bossBarList[player.name]!!)
+							BossbarList.taskID.remove(player.name)
+						}
+						catch(exception: Exception) {
+							println("${player.name}의 bossBar를 찾을 수 없습니다.")
+						}
+					}
+				}
 				args[0] == "getOnlinePlayers" -> {
 					var count = 0
 					for(player in Bukkit.getOnlinePlayers()) {
